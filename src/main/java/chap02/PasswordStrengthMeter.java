68b9cd877f61f9b34b905853c353c48b087c1eb2
matt <yoodjin2@gmail.com>
2021-02-07 19:39:41 +0900

    add: 숫자를 포함하지 않고 조건이 충족되는 유형 추가

--- src/main/java/chap02/PasswordStrengthMeter.java
+++ src/main/java/chap02/PasswordStrengthMeter.java
@@ -4,10 +4,29 @@ public class PasswordStrengthMeter {
 
     public PasswordStrength meter(String password) {
 
+        //8글자 미만 체크
         if (password.length() <= 8) {
             return PasswordStrength.NORMAL;
         }
 
+        //숫자 포함 유무 체크
+        boolean containsNum = meetsContainingNumberCriteria(password);
+
+        if (!containsNum) {
+            return PasswordStrength.NORMAL;
+        }
+
         return PasswordStrength.STRONG;
+    }
+
+    private boolean meetsContainingNumberCriteria(String password) {
+
+        for (char c : password.toCharArray()) {
+            if (c >= '0' && c <= '9') {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
