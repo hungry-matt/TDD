be03b9eacf823691fd2ed10450fc4d75b307059f
matt <yoodjin2@gmail.com>
2021-02-07 19:41:48 +0900

    change: 1. 각 메소드에 중복으로 선언된 PasswordStengthMeter 객체를 전역 필드에서 생성 하도록 수정. 2. DisplayName 애노테이션 추가. 각 테스트 메소드의 이름을 쉽게 정할 수 있다.

--- src/test/java/chap02/PasswordStrengthTest.java
+++ src/test/java/chap02/PasswordStrengthTest.java
@@ -1,26 +1,47 @@
 package chap02;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+//테스트 이름을 원하는 대로 쉽게 정할 수 있다.
+@DisplayName("암호 검사")
 class PasswordStrengthTest {
 
     //모든 조건이 충족 하는 경우
     //모든 조건에 충족 되지 않는 경우
 
+    private PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
     @Test
+    @DisplayName("모든 조건 충족")
     public void 모든_조건_충족시_암호강도_강함() {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
         PasswordStrength result = meter.meter("Aa1#35117");
         assertEquals(PasswordStrength.STRONG, result);
+
     }
 
     @Test
+    @DisplayName("길이가 8글자 미만")
     public void 길이가_8글자_미만시_암호강도_보통() {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
         PasswordStrength result = meter.meter("Aa1#35");
         assertEquals(PasswordStrength.NORMAL, result);
+
+        PasswordStrength result2 = meter.meter("aaa124");
+        assertEquals(PasswordStrength.NORMAL, result2);
+
+    }
+
+    @Test
+    @DisplayName("숫자를 포함하지 않고 조건 충족")
+    public void 숫자를_포함하지_않고_조건_충족시_암호강도_보통() {
+
+        PasswordStrength result = meter.meter("aa@#aksaa");
+        assertEquals(PasswordStrength.NORMAL, result);
+
     }
 
 }
