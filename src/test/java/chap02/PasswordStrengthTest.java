c842afc902afe94b9f41987a61c61ae601d619cd
matt <yoodjin2@gmail.com>
2021-02-07 20:00:40 +0900

    change: 중복 코드 정리

--- src/test/java/chap02/PasswordStrengthTest.java
+++ src/test/java/chap02/PasswordStrengthTest.java
@@ -14,34 +14,28 @@ class PasswordStrengthTest {
 
     private PasswordStrengthMeter meter = new PasswordStrengthMeter();
 
+    private void assertStrength(String password, PasswordStrength expStr) {
+        PasswordStrength result = meter.meter(password);
+        assertEquals(expStr, result);
+    }
+
     @Test
     @DisplayName("모든 조건 충족")
     public void 모든_조건_충족시_암호강도_강함() {
-
-        PasswordStrength result = meter.meter("Aa1#35117");
-        assertEquals(PasswordStrength.STRONG, result);
-
+        assertStrength("Aa1#35117", PasswordStrength.STRONG);
     }
 
     @Test
     @DisplayName("길이가 8글자 미만")
     public void 길이가_8글자_미만시_암호강도_보통() {
-
-        PasswordStrength result = meter.meter("Aa1#35");
-        assertEquals(PasswordStrength.NORMAL, result);
-
-        PasswordStrength result2 = meter.meter("aaa124");
-        assertEquals(PasswordStrength.NORMAL, result2);
-
+        assertStrength("Aa1#35", PasswordStrength.NORMAL);
+        assertStrength("aaa124", PasswordStrength.NORMAL);
     }
 
     @Test
     @DisplayName("숫자를 포함하지 않고 조건 충족")
     public void 숫자를_포함하지_않고_조건_충족시_암호강도_보통() {
-
-        PasswordStrength result = meter.meter("aa@#aksaa");
-        assertEquals(PasswordStrength.NORMAL, result);
-
+        assertStrength("aa@#aksaa", PasswordStrength.NORMAL);
     }
 
 }
