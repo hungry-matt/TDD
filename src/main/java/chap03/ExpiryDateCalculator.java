639b30b00ead04a8a08f70cc351060d92c640b68
matt <yoodjin2@gmail.com>
2021-02-23 13:38:10 +0900

    refactor: 요건이 추가됨에 따라 납부금액에 비례한 개월수 계산 로직이 점점 복잡해져 메소드 추출을 하였고 상수를 정적변수로 선언하여 가독성을 높이도록 적용

--- src/main/java/chap03/ExpiryDateCalculator.java
+++ src/main/java/chap03/ExpiryDateCalculator.java
@@ -5,15 +5,39 @@ import java.time.YearMonth;
 
 public class ExpiryDateCalculator {
 
+    private static int DEFAULT_SERVICE_MONTHS = 12; //10만 원 납부시 제공되는 월 수
+    private static int DEFAULT_SERVICE_BASE_AMOUNT = 100_000; //일정 금액 이상 납부시 제공 되는 월 수의 기준 금액
+    private static int DEFAULT_MONTHLY_PAY_AMOUNT = 10_000; //서비스의 한달 이용료
+
     public LocalDate calculateExpiryDate(PayData payData) {
-        //서비스 한달 이용료는 만원이므로 납부 금액에 비례해서 개월수 계산함
-        int addedMonths = payData.getPayAmount() / 10_000;
+        int addedMonths = calculateMonth(payData);
 
         if (payData.getFirstBillingDate() != null) {
             return expiryDateUsingFirstBillingDate(payData, addedMonths);
         } else {
             return payData.getBillingDate().plusMonths(addedMonths);
         }
+    }
+
+    //서비스 한달 이용료는 만원이므로 납부 금액에 비례해서 개월수 계산함
+    //10만원을 납부하면 1년 제공
+    public int calculateMonth(PayData payData) {
+        int months = 0;
+
+        if (payData.getPayAmount() > DEFAULT_SERVICE_BASE_AMOUNT) {
+            int balance = payData.getPayAmount() - DEFAULT_SERVICE_BASE_AMOUNT;
+            months = DEFAULT_SERVICE_MONTHS + convertMonths(PayData.builder().payAmount(balance).build());
+        } else if (payData.getPayAmount() == DEFAULT_SERVICE_BASE_AMOUNT) {
+            months = DEFAULT_SERVICE_MONTHS;
+        } else {
+            months = convertMonths(payData);
+        }
+
+        return months;
+    }
+
+    public int convertMonths(PayData payData) {
+        return payData.getPayAmount() / DEFAULT_MONTHLY_PAY_AMOUNT;
     }
 
     private LocalDate expiryDateUsingFirstBillingDate(PayData payData, int addedMonths) {
@@ -49,6 +73,7 @@ public class ExpiryDateCalculator {
         return firstBillingDate.getDayOfMonth() != candidate.getDayOfMonth();
     }
 
+    //월의 마지막 일자 반환
     private int lastDayOfMonth(LocalDate candidate) {
         return YearMonth.from(candidate).lengthOfMonth();
     }
